ee3f3095e0db70f502bc2dc4c09523ea38f42cca
Gazi Shoaib <81495143+gazishoaib33@users.noreply.github.com>
2026-02-03 01:08:47 +0600

    Time: 53 ms (96.30%) | Memory: 41 MB (74.07%) - LeetSync

---
+++ 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.py
@@ -0,0 +1,14 @@
+class DataStream(object):
+
+    def __init__(self, value, k):
+        self.value = value
+        self.k = k
+        self.count = 0   # count of consecutive `value`
+
+    def consec(self, num):
+        if num == self.value:
+            self.count += 1
+        else:
+            self.count = 0
+
+        return self.count >= self.k
